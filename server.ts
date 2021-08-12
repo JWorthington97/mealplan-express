163194c9ebb1f04f33f456f127d793ffd42fb0c0
JWorthington97 <jb.worthington@live.co.uk>
2021-08-12 15:38:51 +0100

    Recipe routes and specials routes

--- server.ts
+++ server.ts
@@ -2,6 +2,9 @@ import { Client } from "pg";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
+import dayjs from 'dayjs'
+import isoWeek from 'dayjs/plugin/isoWeek'
+dayjs.extend(isoWeek)
 
 config(); //Read .env file lines as though they were env vars.
 
@@ -26,12 +29,6 @@ app.use(cors()) //add CORS support to each following route handler
 const client = new Client(dbConfig);
 client.connect();
 
-app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
-});
-
-
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
@@ -40,3 +37,131 @@ if (!port) {
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
+
+// Routes
+
+//Cuisines
+app.get("/cuisines", async (req, res) => {
+  try {
+    const dbres = await client.query('SELECT * FROM cuisines');
+    res.json(dbres.rows);
+  }
+  catch (ex) {
+    res.json({
+      status: ex.message
+    })
+  }
+});
+
+app.post("/cuisines", async (req, res) => {
+  // try await client.query("INERT INTO cuisines (cuisine) VALUES ($1), $1.toLowerCase())
+  // catch unable to insert ex.message (will be duplicate)
+  res.json({message: "Creating new cuisines is not currently implemented."})
+})
+
+// Recipes
+app.get("/recipes", async (req, res) => {
+  console.log(dayjs().isoWeek())
+  try {
+    // const dbres = await client.query("SELECT * FROM recipes")
+    const dbres = await client.query(`
+    SELECT 
+      r.id,
+      r.name,
+      c.cuisine,
+      r.url,
+      r.image_url,
+      string_agg(tag, ', ') AS tags
+    FROM recipes r
+      INNER JOIN recipe_tags rt
+        ON rt.recipe_id = r.id
+      INNER JOIN tags t
+        ON t.id = rt.tag_id
+      INNER JOIN cuisines c
+        ON c.id = r.cuisine_id
+    GROUP BY 
+      r.id,
+      r.name,
+      c.cuisine,
+      r.url,
+        r.image_url;`
+        )
+    res.json(dbres.rows)
+  }
+  catch (ex) {
+    res.json({
+      status: ex.message
+    })
+  }
+})
+
+app.post("/recipes", async (req, res) => {
+  // provide a JSON body here
+  // TO LOWER CASE TOO
+  res.json({
+    status: "Adding a new recipe is not currently implemented."
+  })
+})
+
+// Specials
+app.get("/specials", async (req, res) => {
+  try {
+    const {rows} = await client.query(`
+    SELECT 
+      r.id,
+      r.name,
+      c.cuisine,
+      r.url,
+        r.image_url,
+      string_agg(tag, ', ') AS tags
+    FROM recipes r
+      INNER JOIN recipe_tags rt
+        ON rt.recipe_id = r.id
+      INNER JOIN tags t
+        ON t.id = rt.tag_id
+      INNER JOIN cuisines c
+        ON c.id = r.cuisine_id
+        INNER JOIN specials s
+          ON s.recipe_id = r.id
+        WHERE s.week = $1 AND s.year = $2
+    GROUP BY 
+      r.id,
+      r.name,
+      c.cuisine,
+      r.url,
+        r.image_url;`, [dayjs().isoWeek(), dayjs().year()]
+        )
+      res.status(200).json(rows)
+  }
+  catch (ex) {
+    console.log(ex.message)
+    res.status(400).json({
+      message: "unable to retrieve specials"
+    })
+  }
+})
+
+//Recipe Tags
+app.get("/recipetags", async (req, res) => {
+  try {
+    const dbres = await client.query(`
+    SELECT t.tag
+    FROM recipes r
+      INNER JOIN recipe_tags rt
+        ON rt.recipe_id = r.id
+      INNER JOIN tags t
+        ON t.id = rt.tag_id
+    WHERE r.id = $1;`, [req.body.recipe_id])
+    if (dbres.rows.length > 0) {
+      res.json(dbres.rows)
+    }
+    else {
+      res.json({message: "No results found."})
+    }
+  }
+  catch (ex) {
+    res.json({
+      status: ex.message
+    })
+  }
+})
